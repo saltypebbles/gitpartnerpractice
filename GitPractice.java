d48c1b0acb3609ce5962c5a7b4a303ef80ff0f43
AahanaG411 <aahanagorantla@gmail.com>
2024-10-28 13:44:05 -0400

    made square array method

--- GitPractice.java
+++ GitPractice.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class GitPractice
 {
+	private static int[][] arr;
 	public static void main(String[] args)
 	{
 		System.out.println("nandhuuu");
+
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num = reader.nextInt();
+		squareArray(num);
+	}
+
+	public static void squareArray(int n)
+	{
+		arr = new int[n][n];
+		for (int i = 0; i<n; i++)
+		{
+			for (int j = 0; j<n; j++)
+			{
+				arr[i][j] = (int)(Math.random()*10)+1;
+			}
+		}
 	}
 }
